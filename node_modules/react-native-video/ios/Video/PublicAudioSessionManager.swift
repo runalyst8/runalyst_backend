d4ba33a786f13915073b59d9fdb089c792697de5
edd <elif.ddemircioglu@gmail.com>
2026-03-04 22:04:14 +0300

    unnecessary files deleted

--- node_modules/react-native-video/ios/Video/PublicAudioSessionManager.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-@objc
-public class PublicAudioSessionManager: NSObject {
-    @objc
-    public static func setIsAudioSessionManagementDisabled(_ disabled: Bool) {
-        AudioSessionManager.shared.setIsAudioSessionManagementForcedDisabled(disabled: disabled)
-    }
-}
